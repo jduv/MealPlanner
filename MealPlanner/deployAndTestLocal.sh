ddd51cf652560abf5e6d33fbeb6289da898bd7d5
Jeremy Duvall <matchless.tone@gmail.com>
2013-03-10 21:38:43 -0400

    Adding files...

---
+++ MealPlanner/deployAndTestLocal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DEPLOY_DIR="${HOME}/Sites/MealPlanner"
+
+# Check to see if the directory exists.
+if [ ! -d "$DEPLOY_DIR" ]; then
+	mkdir "$DEPLOY_DIR"
+fi
+
+# Copy everything over.
+cp Ui/*.* "$DEPLOY_DIR"
+
+open -a "Google Chrome" "http://localhost/~jduv/MealPlanner/index.html"
