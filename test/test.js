8fe4a69994cadf0d68988bbee12ab80e27d571e4
jduv <matchless.tone@gmail.com>
2013-03-26 18:15:55 -0400

    Adding more experimental tests.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,34 @@
 // Require tested modules
-require('../Ui/js/MealPlanner-1.0.js');
+require('../Ui/js/mealplanner-1.0.js');
 var chai = require('chai');
 
 suite('Array', function(){
-  setup(function(){
-  	// ...
-  });
-
   suite('#indexOf()', function(){
     test('should return -1 when not present', function(){
       chai.assert.strictEqual(-1, [1,2,3].indexOf(4));
     });
   });
+});
+
+suite('test', function() {
+
+	var i = 0;
+	var g = {};
+
+	setup(function() {
+		console.log('in setup');
+	 	g = { a : 'test', b : i++ };
+	});
+
+	console.log(g);
+
+	test('should work', function() {
+		chai.assert.strictEqual(g.a, 'test');
+		chai.assert.strictEqual(g.b, 0);
+	});
+
+	test('should also work', function() {
+		chai.assert.strictEqual(g.a, 'test');
+		chai.assert.strictEqual(g.b, 1);
+	});
 });
